58cad2d1ea31dd6936ddab59de6bbed51f580724
gojutin <gojutin@gmail.com>
2021-11-25 17:32:07 -0500

    style: component updates

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,3 +1,5 @@
+import { FaUser } from "react-icons/fa";
+
 export type LayoutProps = {
   /**
    * Optionally render children
@@ -10,11 +12,21 @@ export const Layout = (props: LayoutProps) => {
   const headerFooterClassNames = "text2 surface2 p-4";
   return (
     <div className="flex flex-col min-h-screen">
-      <header className={headerFooterClassNames}>Header</header>
+      <header className={headerFooterClassNames}>
+        <div className="flex items-center justify-between">
+          <h1 className="font-thin text-2xl">Next.js Starter</h1>
+          <div className="rounded-full surface1 items-center justify-center p-2">
+            <FaUser />
+          </div>
+        </div>
+      </header>
       <main className="flex-1">
         <div className="container mx-auto p-4">{children || "My New Page"}</div>
       </main>
-      <footer className={headerFooterClassNames}>Footer</footer>
+      <footer
+        className={`flex items-center justify-center ${headerFooterClassNames}`}>
+        Next.js Starter | {new Date().getFullYear()}
+      </footer>
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { Layout } from "components/Layout";
 import { SEO } from "components/SEO";
 import { Emoji } from "components/Emoji";
-import { Box } from "components/Box";
 
 const App = () => {
   return (
@@ -10,9 +9,11 @@ const App = () => {
         title="Next.js Starter"
         description="A Next.js starter with TypeScript, TailwindCSS, and more."
       />
-      <Box as="span" className="flex gap-2">
-        <Emoji symbol="👋" label="Hello" /> <Emoji symbol="🌎" label="World" />
-      </Box>
+      <div>
+        <p className="text-3xl text2">
+          <Emoji symbol="👋" label="Hello" /> Hey
+        </p>
+      </div>
     </Layout>
   );
 };
