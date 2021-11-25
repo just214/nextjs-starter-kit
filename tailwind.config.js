5627c14cd13f52431caba01ab73a4c5db79fa0cb
gojutin <gojutin@gmail.com>
2021-11-25 17:31:51 -0500

    style: updated to Tailwind v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
-const colors = require("tailwindcss/colors");
-
 module.exports = {
-  mode: "jit",
-  purge: ["./src/**/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/**/*.{js,ts,jsx,tsx}"],
   darkMode: "media", // 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        blueGray: colors.blueGray,
         transblack: "rgba(0,0,0,.1)",
       },
     },
